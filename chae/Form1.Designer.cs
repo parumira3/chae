db5c7cde9f9dc481bdd843f93fb0e40343501936
parumira3 <k20753014@gmail.com>
2019-08-27 18:05:23 +0900

    fist

--- chae/Form1.Designer.cs
+++ chae/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictNone = new System.Windows.Forms.PictureBox();
+            this.pictStar = new System.Windows.Forms.PictureBox();
+            this.pictDal = new System.Windows.Forms.PictureBox();
+            this.pictHae = new System.Windows.Forms.PictureBox();
+            this.rdoPlayer1 = new System.Windows.Forms.RadioButton();
+            this.rdoPlayer2 = new System.Windows.Forms.RadioButton();
+            this.listResult1 = new System.Windows.Forms.ListBox();
+            this.listResult2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictNone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictStar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictDal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictHae)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "각 플레이어는 한번식 돌아가면서 그림을 선택합니다";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "플레이어는 각 5회 진행";
+            // 
+            // pictNone
+            // 
+            this.pictNone.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictNone.Location = new System.Drawing.Point(406, 62);
+            this.pictNone.Name = "pictNone";
+            this.pictNone.Size = new System.Drawing.Size(120, 119);
+            this.pictNone.TabIndex = 5;
+            this.pictNone.TabStop = false;
+            this.pictNone.Click += new System.EventHandler(this.pictNone_Click);
+            // 
+            // pictStar
+            // 
+            this.pictStar.Image = global::chae.Properties.Resources._3;
+            this.pictStar.Location = new System.Drawing.Point(280, 62);
+            this.pictStar.Name = "pictStar";
+            this.pictStar.Size = new System.Drawing.Size(120, 119);
+            this.pictStar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictStar.TabIndex = 4;
+            this.pictStar.TabStop = false;
+            this.pictStar.Click += new System.EventHandler(this.pictStar_Click);
+            // 
+            // pictDal
+            // 
+            this.pictDal.Image = global::chae.Properties.Resources._2;
+            this.pictDal.Location = new System.Drawing.Point(154, 62);
+            this.pictDal.Name = "pictDal";
+            this.pictDal.Size = new System.Drawing.Size(120, 119);
+            this.pictDal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictDal.TabIndex = 3;
+            this.pictDal.TabStop = false;
+            this.pictDal.Click += new System.EventHandler(this.pictDal_Click);
+            // 
+            // pictHae
+            // 
+            this.pictHae.Image = global::chae.Properties.Resources._1;
+            this.pictHae.Location = new System.Drawing.Point(28, 62);
+            this.pictHae.Name = "pictHae";
+            this.pictHae.Size = new System.Drawing.Size(120, 119);
+            this.pictHae.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictHae.TabIndex = 2;
+            this.pictHae.TabStop = false;
+            this.pictHae.Click += new System.EventHandler(this.pictHae_Click);
+            // 
+            // rdoPlayer1
+            // 
+            this.rdoPlayer1.AutoSize = true;
+            this.rdoPlayer1.Location = new System.Drawing.Point(128, 237);
+            this.rdoPlayer1.Name = "rdoPlayer1";
+            this.rdoPlayer1.Size = new System.Drawing.Size(77, 16);
+            this.rdoPlayer1.TabIndex = 6;
+            this.rdoPlayer1.TabStop = true;
+            this.rdoPlayer1.Text = "플레이어1";
+            this.rdoPlayer1.UseVisualStyleBackColor = true;
+            // 
+            // rdoPlayer2
+            // 
+            this.rdoPlayer2.AutoSize = true;
+            this.rdoPlayer2.Location = new System.Drawing.Point(318, 237);
+            this.rdoPlayer2.Name = "rdoPlayer2";
+            this.rdoPlayer2.Size = new System.Drawing.Size(77, 16);
+            this.rdoPlayer2.TabIndex = 7;
+            this.rdoPlayer2.TabStop = true;
+            this.rdoPlayer2.Text = "플레이어2";
+            this.rdoPlayer2.UseVisualStyleBackColor = true;
+            // 
+            // listResult1
+            // 
+            this.listResult1.FormattingEnabled = true;
+            this.listResult1.ItemHeight = 12;
+            this.listResult1.Location = new System.Drawing.Point(12, 281);
+            this.listResult1.Name = "listResult1";
+            this.listResult1.Size = new System.Drawing.Size(251, 136);
+            this.listResult1.TabIndex = 8;
+            // 
+            // listResult2
+            // 
+            this.listResult2.FormattingEnabled = true;
+            this.listResult2.ItemHeight = 12;
+            this.listResult2.Location = new System.Drawing.Point(292, 281);
+            this.listResult2.Name = "listResult2";
+            this.listResult2.Size = new System.Drawing.Size(251, 136);
+            this.listResult2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(555, 450);
+            this.Controls.Add(this.listResult2);
+            this.Controls.Add(this.listResult1);
+            this.Controls.Add(this.rdoPlayer2);
+            this.Controls.Add(this.rdoPlayer1);
+            this.Controls.Add(this.pictNone);
+            this.Controls.Add(this.pictStar);
+            this.Controls.Add(this.pictDal);
+            this.Controls.Add(this.pictHae);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictNone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictStar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictDal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictHae)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictHae;
+        private System.Windows.Forms.PictureBox pictDal;
+        private System.Windows.Forms.PictureBox pictStar;
+        private System.Windows.Forms.PictureBox pictNone;
+        private System.Windows.Forms.RadioButton rdoPlayer1;
+        private System.Windows.Forms.RadioButton rdoPlayer2;
+        private System.Windows.Forms.ListBox listResult1;
+        private System.Windows.Forms.ListBox listResult2;
     }
 }
 
--- chae/Form1.cs
+++ chae/Form1.cs
@@ -12,9 +12,146 @@ namespace chae
 {
     public partial class Form1 : Form
     {
+        struct structPlayer
+        {
+            public int iCount;
+
+            public int iSun;
+            public int iDal;
+            public int iStar;
+
+            public int icardSum;
+
+            public int CardSum(int iSun, int iDal, int iStar)
+            {
+                return iSun + iDal + iStar;
+            }
+
+            public string ResultText()
+            {
+                return string.Format("{0}회 해:{1}, 달:{2}, 별:{3} => 합계는 {4}입니다", iCount, iSun, iDal, iStar, icardSum);
+            }
+
+
+        }
+
+        //class classPlayer
+        //{
+        //    public int iCount = 0;
+
+        //    public int iSun;
+        //    public int iDal;
+        //    public int iStar;
+
+        //    public int icardSum;
+
+        //    public int CardSum(int iSun, int iDal, int iStar)
+        //    {
+        //        return iSun + iDal + iStar;
+        //    }
+
+        //    public string ResultText()
+        //    {
+        //        return string.Format("{0}회 해:{1}, 달:{2}, 별:{3} => 합계는 {4}입니다", iCount, iSun, iDal, iStar, icardSum);
+        //    }
+
+        //}
+
+        structPlayer _stPlayer1;
+        structPlayer _stPlayer2;
+
+        Random _rd = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+
+
+        private void pictHae_Click(object sender, EventArgs e)
+        {
+                        
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _stPlayer1.iSun = iNumber;
+            }
+            else
+            {
+                _stPlayer2.iSun = iNumber;
+            }
+
+            Result();
+            iCheckedChange();
+
+        }
+
+        private void pictDal_Click(object sender, EventArgs e)
+        {
+
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _stPlayer1.iDal = iNumber;
+            }
+            else
+            {
+                _stPlayer2.iDal = iNumber;
+            }
+            iCheckedChange();
+
+        }
+
+        private void pictStar_Click(object sender, EventArgs e)
+        {
+
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _stPlayer1.iStar = iNumber;
+            }
+            else
+            {
+                _stPlayer2.iStar = iNumber;
+            }
+            iCheckedChange();
+        }
+
+        private void pictNone_Click(object sender, EventArgs e)
+        {
+            iCheckedChange();
+        }
+
+        private void iCheckedChange()
+        {
+            if (rdoPlayer1.Checked)
+            {
+                rdoPlayer2.Checked = true;
+            }
+            else
+            {
+                rdoPlayer1.Checked = true;
+            }
+        }
+
+        private void Result()
+        {
+            string strResult = string.Empty;
+
+            int iCardSum = 0;
+
+            if (rdoPlayer1.Checked)
+            {
+                _stPlayer1.iCount++;
+                _stPlayer1.icardSum =  _stPlayer1.CardSum(_stPlayer1.iSun, _stPlayer1.iDal, _stPlayer1.iStar);
+                strResult = _stPlayer1.ResultText();
+            }
         }
     }
 }
